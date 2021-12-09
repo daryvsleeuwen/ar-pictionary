7bd7f779bbfbc27d00668941a07f4270c6ed630c
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2021-12-09 10:50:15 +0100

    Swift app setup

---
+++ AR-pictionary/AR_pictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_pictionaryApp.swift
+//  AR-pictionary
+//
+//  Created by Dary van Sleeuwen on 09/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AR_pictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR-pictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AR-pictionary
+//
+//  Created by Dary van Sleeuwen on 09/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
